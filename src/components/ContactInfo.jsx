c76e5be3fbab4b5f2e5bc781619bb87a5e8b200e
Abiodn Olutoye <abiodunolutoye1912@gmail.com>
2025-08-28 18:30:35 +0100

    fix: update phone number format and correct website rendering in ContactInfo component

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -4,13 +4,13 @@ export default function ContactInfo() {
   return (
     <div className="mt-4 space-y-2 text-gray-600">
       <p className="flex items-center justify-center">
-        <FaPhone className="mr-2 text-indigo-500" /> +234 801 234 5678
+        <FaPhone className="mr-2 text-indigo-500" /> +2347068364743
       </p>
       <p className="flex items-center justify-center">
         <FaEnvelope className="mr-2 text-indigo-500" /> olegitng@gmail.com
       </p>
       <p className="flex items-center justify-center">
-        <FaGlobe className="mr-2 text-indigo-500" /> <www className="olegit com ng"></www>
+        <FaGlobe className="mr-2 text-indigo-500" />olegit.com.ng
       </p>
     </div>
   );
